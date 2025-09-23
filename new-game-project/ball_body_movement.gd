19f27f18b725d8a725692e4829e1c2cba358a4ab
@ishmael.mendez <ishmael.mendez@us.af.mil>
2025-09-23 13:14:40 -0500

    made some small edits concerning the ball color

--- new-game-project/ball_body_movement.gd
+++ new-game-project/ball_body_movement.gd
@@ -2,17 +2,11 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	velocity += get_gravity() * delta
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
 
 	move_and_slide()
