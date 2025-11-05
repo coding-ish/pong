ed7ee7c1bfa1f27b26444bb97e6f395fa65b5c90
codah <102069397+coding-ish@users.noreply.github.com>
2025-11-05 12:46:07 -0600

    Update left_paddle_movement.gd
    removed git lingo

--- new-game-project/left_paddle_movement.gd
+++ new-game-project/left_paddle_movement.gd
@@ -3,7 +3,6 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 
-<<<<<<< HEAD
 func get_input():
 	var input_direction = Input.get_vector("ui_left","ui_right","up_left_paddle","down_left_paddle")
 
@@ -26,14 +25,10 @@ func _process(delta):
 		
 func get_input():
 	var input_direction = Input.get_vector("ui_left","ui_right","up_left_paddle","down_left_paddle")
->>>>>>> b83d7cf32b1902b9a9ee644dbf8c616a78f420c6
 	velocity = input_direction * SPEED
 	
 func _physics_process(delta):
 	_process(delta)
 	get_input()
-<<<<<<< HEAD
 
 	move_and_slide()	
-=======
->>>>>>> b83d7cf32b1902b9a9ee644dbf8c616a78f420c6
